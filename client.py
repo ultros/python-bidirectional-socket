fafdb8d05355ad575fede8bee5d91ebcf5459091
Jesse Shelley <jesse@cybertutorials.org>
2022-04-27 11:34:47 -0600

    Added encryption to messages.

--- client.py
+++ client.py
@@ -1,9 +1,16 @@
-import socket
 
+import socket
+from cryptography.fernet import Fernet
+
+key = b'p7GvOQ_uEvKUPfeWPCeXG7LQyP0LpqMp09qbW7xCxps='
+f = Fernet(key)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('127.0.0.1', 8888))
+sock.send(f.encrypt(b'test from client'))
 
-sock.send("Sent from client...".encode())
-print(sock.recv(1024).decode())
+data = sock.recv(1024)
+plaintext = f.decrypt(data)
+print(plaintext.decode())
+
 sock.close()
--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 import socket
+from cryptography.fernet import Fernet
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 8888))
 sock.listen()
+key = b'p7GvOQ_uEvKUPfeWPCeXG7LQyP0LpqMp09qbW7xCxps='
+f = Fernet(key)
 
 while True:
     connect, addr = sock.accept()
-    connect.send("You've connected....".encode())
+    connect.send(f.encrypt(b"You've connected...."))
+
     data = connect.recv(1024)
-    print(data.decode())
+    plaintext = f.decrypt(data)
+    print(plaintext.decode())
     connect.close()
