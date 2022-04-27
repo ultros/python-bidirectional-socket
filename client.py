0f9dc4da10e138941d268ec64f5f4319581a5108
Jesse Shelley <jesse@cybertutorials.org>
2022-04-27 12:09:01 -0600

    Added encryption to messages.

--- client.py
+++ client.py
@@ -1,16 +1,19 @@
-
 import socket
 from cryptography.fernet import Fernet
 
+# The key generated below is the shared symmetric "secret key" used to encrypt and decrypt traffic between the hosts.
+# This key is shared on the host and the client.
 key = b'p7GvOQ_uEvKUPfeWPCeXG7LQyP0LpqMp09qbW7xCxps='
 f = Fernet(key)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('127.0.0.1', 8888))
-sock.send(f.encrypt(b'test from client'))
+sock.send(f.encrypt(b'test from client')) # here we have encrypted a message being sent to the server
 
 data = sock.recv(1024)
-plaintext = f.decrypt(data)
+plaintext = f.decrypt(data) # here we decrypt a message we receive from the server
 print(plaintext.decode())
 
 sock.close()
+
+print(f.decrypt(b"gAAAAABiaX46nQw0Nz4AFHLlRNTSSr9htfR8fYY85pF1OQUROT87z-4S6LHiIxDxOXMRWaoDbPVIPX4G3iuFVty2RfvHSiXKFaeDnYZh3aVH2VGRYvkN-Qo="))
--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 import socket
 from cryptography.fernet import Fernet
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('127.0.0.1', 8888))
-sock.listen()
+
 key = b'p7GvOQ_uEvKUPfeWPCeXG7LQyP0LpqMp09qbW7xCxps='
 f = Fernet(key)
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('127.0.0.1', 8888))
+sock.listen()
+
+
 while True:
     connect, addr = sock.accept()
-    connect.send(f.encrypt(b"You've connected...."))
+    connect.send(f.encrypt(b"You've connected....")) # here we encrypt a message before sending it to the client
 
     data = connect.recv(1024)
-    plaintext = f.decrypt(data)
+    plaintext = f.decrypt(data) # here we decrypt the ciphertext sent from the client
     print(plaintext.decode())
     connect.close()
